1076e75b6bf10f42ffb1755d12899e9c00609382
jpawlow2 <116225981+jpawlow2@users.noreply.github.com>
2024-10-06 16:17:42 -0700

    Update StartCSE360.java
    admin startup success, most of login fully implemented

--- StartCSE360.java
+++ StartCSE360.java
@@ -1,10 +1,12 @@
 package simpleDatabase;
 
+import java.sql.*;
 import java.sql.SQLException;
 import java.util.Scanner;
 
 public class StartCSE360 {
 
+	
 	private static final DatabaseHelper databaseHelper = new DatabaseHelper();
 	private static final Scanner scanner = new Scanner(System.in);
 
@@ -12,7 +14,6 @@ public class StartCSE360 {
 	{
 
 		try { 
-			
 			databaseHelper.connectToDatabase();  // Connect to the database
 
 			// Check if the database is empty (no users registered)
@@ -22,25 +23,7 @@ public class StartCSE360 {
 				setupAdministrator();
 			}
 			else {
-				System.out.println( "If you are an administrator, then select A\nIf you are an instructor then select I\n"
-						+ "If you are a user, then select U\nEnter your choice:  " );
-				String role = scanner.nextLine();
-
-				switch (role) {
-				case "U":
-					userFlow();
-					break;
-				case "A":
-					adminFlow();
-					break;
-				case "I":
-					instructorFlow();
-					break;
-				default:
-					System.out.println("Invalid choice. Please select 'a', 'i', 'u'");
-					databaseHelper.closeConnection();
-				}
-
+				normalStartup();
 			}
 		} catch (SQLException e) {
 			System.err.println("Database error: " + e.getMessage());
@@ -56,9 +39,14 @@ public class StartCSE360 {
 		boolean matched = false;
 		String passwordfirst = null;
 		String password = null;
+		String email = null;
+		String fullName = null;
+		String prefName = null;
+		Date expire = null;
+		String skillLevel = "Intermediate";
 		System.out.println("Setting up the Administrator access.");
-		System.out.print("Enter Admin Email: ");
-		String email = scanner.nextLine();
+		System.out.print("Enter Admin Username: ");
+		String username = scanner.nextLine();
 		while(!matched) {
 			System.out.print("Enter Admin Password: ");
 			passwordfirst = scanner.nextLine();
@@ -70,27 +58,55 @@ public class StartCSE360 {
 				matched = true;
 			}
 		}
-		databaseHelper.register(email, password, "admin");
+		databaseHelper.register(username, password, "admin", email, fullName, prefName, false, expire, skillLevel);
 		System.out.println("Administrator setup completed.");
+		normalStartup();
 
 	}
 
-	private static void userFlow() throws SQLException {
+	private static void normalStartup() throws SQLException {
+		System.out.println( "If you are an administrator, then type A\nIf you are an instructor then type I\n"
+				+ "If you are a Student, then type S\nEnter your choice:  " );
+		String role = scanner.nextLine();
+
+		switch (role) {
+		case "S":
+			studentFlow();
+			break;
+		case "A":
+			adminFlow();
+			break;
+		case "I":
+			instructorFlow();
+			break;
+		default:
+			System.out.println("Invalid choice. Please select 'A', 'I', 'S'");
+			databaseHelper.closeConnection();
+		}
+	}
+	
+	private static void studentFlow() throws SQLException {
 		String email = null;
 		String passwordfirst = null;
 		String password = null;
 		boolean matched = false;
-		System.out.println("user flow");
+		String fullName = null;
+		String prefName = null;
+		Date expire = null;
+		String skillLevel = "Intermediate";
+		System.out.println("student flow");
 		System.out.print("What would you like to do 1.Register 2.Login  ");
 		String choice = scanner.nextLine();
 		switch(choice) {
 		case "1": 
-			System.out.print("Enter User Email: ");
+			System.out.print("Enter Student Username: ");
+			String username = scanner.nextLine();
+			System.out.print("Enter Student Email: ");
 			email = scanner.nextLine();
 			while(!matched) {
-				System.out.print("Enter User Password: ");
+				System.out.print("Enter Student Password: ");
 				passwordfirst = scanner.nextLine(); 
-				System.out.print("Enter User Password Again: ");
+				System.out.print("Enter Student Password Again: ");
 				password = scanner.nextLine(); 
 				if(password.compareTo(passwordfirst) != 0) {
 					System.out.print("ERROR : Passwords must match.\n");
@@ -98,23 +114,53 @@ public class StartCSE360 {
 					matched = true;
 				}
 			}
+			System.out.print("Enter Student First Name: ");
+			String first = scanner.nextLine();
+			System.out.print("Enter Student Middle Name: ");
+			String middle = scanner.nextLine();
+			System.out.print("Enter Student Last Name: ");
+			String last = scanner.nextLine();
+			System.out.print("Enter Student Preferred Name (Enter to Skip): ");
+			prefName = scanner.nextLine();
+			if(prefName.compareTo("") == 0) {
+				prefName = "N/A";
+			}
+			fullName = first + " " + middle + " " + last;
+			
 			// Check if user already exists in the database
-		    if (!databaseHelper.doesUserExist(email)) {
-		        databaseHelper.register(email, password, "user");
+		    if (!databaseHelper.doesUserExist(username)) {
+		    	boolean logout = false;
+		    	databaseHelper.register(username, password, "student", email , fullName, prefName, false, expire, skillLevel);
 		        System.out.println("User setup completed.");
+		        System.out.println(" ------------------- ");
+		        System.out.println("Logout at anytime by typing 'logout'.");
+		        while(!logout) {
+		        	 String input = scanner.nextLine();
+				        if(input.compareTo("logout") == 0) {
+				        	logout = true;
+				        }
+		        }
 		    } else {
 		        System.out.println("User already exists.");
 		    }
 			break;
 		case "2":
-			System.out.print("Enter User Email: ");
-			email = scanner.nextLine();
-			System.out.print("Enter User Password: ");
+			System.out.print("Enter Student Username: ");
+			username = scanner.nextLine();
+			System.out.print("Enter Student Password: ");
 			password = scanner.nextLine();
-			if (databaseHelper.login(email, password, "user")) {
+			if (databaseHelper.login(username, password, "student")) {
 				System.out.println("User login successful.");
 //				databaseHelper.displayUsers();
-
+				 System.out.println(" ------------------- ");
+			        System.out.println("Logout at anytime by typing 'logout'.");
+			        boolean logout = false;
+			        while(!logout) {
+			        	 String input = scanner.nextLine();
+					        if(input.compareTo("logout") == 0) {
+					        	logout = true;
+					        }
+			        }
 			} else {
 				System.out.println("Invalid user credentials. Try again!!");
 			}
@@ -127,11 +173,17 @@ public class StartCSE360 {
 		String passwordfirst = null;
 		String password = null;
 		boolean matched = false;
+		String fullName = null;
+		String prefName = null;
+		Date expire = null;
+		String skillLevel = "Intermediate";
 		System.out.println("instructor flow");
 		System.out.print("What would you like to do 1.Register 2.Login  ");
 		String choice = scanner.nextLine();
 		switch(choice) {
 		case "1": 
+			System.out.print("Enter Instructor Username: ");
+			String username = scanner.nextLine();
 			System.out.print("Enter Instructor Email: ");
 			email = scanner.nextLine();
 			while(!matched) {
@@ -145,22 +197,49 @@ public class StartCSE360 {
 					matched = true;
 				}
 			}
+			System.out.print("Enter Instructor First Name: ");
+			String first = scanner.nextLine();
+			System.out.print("Enter Instructor Middle Name: ");
+			String middle = scanner.nextLine();
+			System.out.print("Enter Instructor Last Name: ");
+			String last = scanner.nextLine();
+			System.out.print("Enter Instructor Preferred Name (Enter to Skip): ");
+			prefName = scanner.nextLine();
+			if(prefName.compareTo("") == 0) {
+				prefName = "N/A";
+			}
+			fullName = first + " " + middle + " " + last;
+			
 			// Check if user already exists in the database
-		    if (!databaseHelper.doesUserExist(email)) {
-		        databaseHelper.register(email, password, "user");
+		    if (!databaseHelper.doesUserExist(username)) {
+		    	databaseHelper.register(username, password,"instructor", email, fullName, prefName, false, expire, skillLevel);
 		        System.out.println("Instructor setup completed.");
+		        boolean logout = false;
+		        while(!logout) {
+		        	 String input = scanner.nextLine();
+				        if(input.compareTo("logout") == 0) {
+				        	logout = true;
+				        }
+		        }
 		    } else {
 		        System.out.println("Instructor already exists.");
 		    }
 			break;
 		case "2":
-			System.out.print("Enter Instructor Email: ");
-			email = scanner.nextLine();
+			System.out.print("Enter Instructor Username: ");
+			username = scanner.nextLine();
 			System.out.print("Enter Instructor Password: ");
 			password = scanner.nextLine();
-			if (databaseHelper.login(email, password, "user")) {
+			if (databaseHelper.login(username, password, "instructor")) {
 				System.out.println("Instructor login successful.");
 //				databaseHelper.displayUsers();
+				boolean logout = false;
+		        while(!logout) {
+		        	 String input = scanner.nextLine();
+				        if(input.compareTo("logout") == 0) {
+				        	logout = true;
+				        }
+		        }
 
 			} else {
 				System.out.println("Invalid user credentials. Try again!!");
@@ -171,11 +250,11 @@ public class StartCSE360 {
 
 	private static void adminFlow() throws SQLException {
 		System.out.println("admin flow");
-		System.out.print("Enter Admin Email: ");
-		String email = scanner.nextLine();
+		System.out.print("Enter Admin Username: ");
+		String username = scanner.nextLine();
 		System.out.print("Enter Admin Password: ");
 		String password = scanner.nextLine();
-		if (databaseHelper.login(email, password, "admin")) {
+		if (databaseHelper.login(username, password, "admin")) {
 			System.out.println("Admin login successful.");
 			databaseHelper.displayUsersByAdmin();
 
